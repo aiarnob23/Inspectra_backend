2e99578a31d16f589ff5da951cdd834e7f5633c0
aiarnob23 <aiarnob23@gmail.com>
2026-02-05 13:49:34 +0600

    database design updated

---
+++ src/modules/Payment/payment.service.ts
@@ -0,0 +1,18 @@
+
+export default class PaymentService {
+    //ACID
+    //calculation PRORATION
+    //calculation TAX
+    //calculation DISCOUNT
+    //calculation INSURANCE
+    //calculation PREMIUM
+    //calculation NET PREMIUM
+    //
+    //payment process
+    //payment security -
+    //payment confirmation
+    //payment receipt
+    //payment transaction
+    //rollback for craash
+    
+}
